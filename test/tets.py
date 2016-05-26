ac9d35b1a8c5d6599235572b36b6fbe111560424
wangfeng7399 <wangfeng17399@163.com>
2016-05-26 19:51:21 +0800

    新增多线程上传文件

--- test/tets.py
+++ test/tets.py
@@ -25,6 +25,55 @@ b=re.findall('upstream\s*%s\s*\{.*?;\n\}\n'%'oldd',a,re.S)
 print(b[0])
 c=re.sub('(.*back)',lambda m:"#"+m.group(0),b[0])
 print(c)
-'''
+
 import os
-os.path.isdir()
+os.path.exists('/opt/admin/2016-5-26/')'''
+
+from multiprocessing import Pool
+import os,time,random
+def long_time_task(name):
+    print('run task {0} ({1})'.format(name,os.getpid()))
+    start=time.time()
+    time.sleep(random.random()*3)
+    end=time.time()
+    print('task {0} run {1}s'.format(name,(end-start)))
+def a(name):
+    print('run a {0} ({1})'.format(name,os.getpid()))
+    print(1)
+if __name__ == '__main__':
+    print('Parent process {0}'.format(os.getpid()))
+    p=Pool(4)
+    for i in range(5):
+        p.apply_async(long_time_task,args=(i,))
+        p.apply_async(a,args=(i,))
+    print('waiting for all sub done')
+    p.close()
+    p.join()
+    print('all sub done')
+import paramiko
+import os
+"""
+ssh=paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect('10.10.0.53',22,'root',)
+stdin,stdout,stderr=ssh.exec_command('ls')
+for i in stdout.readlines():
+    if os.path.isdir(i):
+        print('daa')
+    else:
+        print('da')
+"""
+"""
+t=paramiko.Transport('10.10.0.53',22)
+t.connect(username='root',password='51credit.com')
+sftp=paramiko.SFTPClient.from_transport(t)
+try:
+    sftp.mkdir('/opt/admin/1',mode=0o755)
+except:
+    pass
+try:
+    sftp.put('/opt/admin/2016-05-26/Hydrangeas.jpg','/opt/admin/2/Hydrangeas.jpg')
+except:
+
+    t.close()
+"""
--- web/code.py
+++ web/code.py
@@ -8,11 +8,21 @@ from django.contrib.auth.models import User
 from .models import Team,Host
 from .base import encode,decode
 import paramiko
-
+import time
+import os
+from multiprocessing import Pool
 
 #上传文件
-def update():
-    pass
+def update(hostip,port,username,password,updatefile,filename):
+    t=paramiko.Transport(hostip,port)
+    t.connect(username=username,password=password)
+    sftp=paramiko.SFTPClient.from_transport(t)
+    try:
+        sftp.mkdir('/update',mode=0o755)
+    except:
+        pass
+    sftp.put(updatefile,'{0}/{1}'.format('/update',filename))
+    t.close()
 
 #摘机器
 def nginx():
@@ -26,19 +36,33 @@ def backup():
 def back():
     pass
 
+#多进程
 
 @login_required(login_url=reverse_lazy('login'))
 def code(request):
     teamall=Team.objects.all()
     if request.method =="POST":
-        filename=request.FILES.get('file')
-        path='{0}/{1}'.format('/root/',filename.name)
-        with open(path,'wb+') as f:
-            for chunk in filename.chunks():
-                f.write(chunk)
-        #cp文件，重启服务，返回结果
-        return HttpResponse("上传完成")
-    return render(request,'updatefile.html',{"teamall":teamall})
+        filename=request.FILES.getlist('files[]')
+        teamname=request.POST.get('teamname')
+        teamhost=Team.objects.get(id=teamname)
+        path='{0}/{1}/{2}'.format('/opt',request.user,time.strftime("%Y-%m-%d",time.localtime()),)
+        if not os.path.exists(path):
+            os.makedirs(path)
+        for file in filename:
+            pathname='{0}/{1}'.format(path,file.name)
+            with open(pathname,'wb+') as f:
+                for chunk in file.chunks():
+                    f.write(chunk)
+            #cp文件，重启服务，返回结果
+            #这里应该用多进程写
+            p=Pool(5)
+            for host in teamhost.host.all():
+                p.apply_async(update,args=(host.hostip,host.port,host.user,decode(host.hostpwd),pathname,file.name))
+            p.close()
+            p.join()
+            return render(request,'upload.html',{"msg":"已经成功上传,正在发布，请关注邮箱，在发布完成，系统会发送邮件给你"})
+
+    return render(request,'upload.html',{"teamall":teamall})
 
 @login_required(login_url=reverse_lazy('login'))
 def status(request):
