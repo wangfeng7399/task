b78f4863040215c7a301f13faa601e271b7dfc88
wangfeng7399 <wangfeng17399@163.com>
2016-05-31 16:23:51 +0800

    新增curl

--- web/code.py
+++ web/code.py
@@ -13,15 +13,12 @@ import os
 from multiprocessing import Pool
 
 class update:
-    def __init__(self,hostip,port,username,password,updatefile,filename,teamnameid,teamhost):
+    def __init__(self,hostip,port,username,password,updatefile,filename):
         self.updatefile=updatefile
         self.filename=filename
-        self.teamnameid=teamnameid
-        self.teamhost=teamhost
         self.t=paramiko.Transport(hostip,port)
         self.t.connect(username=username,password=password)
         self.sftp=paramiko.SFTPClient.from_transport(self.t)
-        self.team=Team.objects.get(id=teamnameid)
         self.ssh=paramiko.SSHClient()
         self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         self.ssh.connect(hostip,port,username,password)
@@ -34,7 +31,7 @@ class update:
         self.t.close()
     #备份
     def backup(self):
-        self.ssh.exec_command('mv -rf {0} {1}/{2}'.format(self.teamhost.host,'/backup',self.teamhost.path+time.strftime("%Y-%m-%d",time.localtime())))
+        pass
     #替换文件
     #回滚
     def goback(self):
@@ -97,7 +94,7 @@ def code(request):
             list.append(file.name)
             p=Pool(5)
             for host in teamhost.host.all():
-                u=update(host.hostip,host.port,host.user,decode(host.hostpwd),pathname,file.name,teamname,teamhost)
+                u=update(host.hostip,host.port,host.user,decode(host.hostpwd),pathname,file.name)
                 p.apply_async(u.update())
             p.close()
             p.join()
@@ -105,6 +102,7 @@ def code(request):
         Code.objects.create(team=teamhost,path=list,status=status,user=userid)
         return render(request,'upload.html',{"msg":"已经上传成功，请前往发布列表页进行发布","teamall":teamall})
     return render(request,'upload.html',{"teamall":teamall})
+
 
 @login_required(login_url=reverse_lazy('login'))
 def status(request):
