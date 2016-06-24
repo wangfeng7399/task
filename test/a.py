5ab790d3653d5524b0072631c12cc58fdf2824ed
wangfeng7399 <wangfeng17399@163.com>
2016-06-24 19:25:45 +0800

    update

--- test/a.py
+++ test/a.py
@@ -17,18 +17,15 @@ def createdict(path):
 c=createdict("/root")
 print(c)
 '''
-list=[]
+
 import  os,json
 def tree(path):
+    list=[]
     for item in os.listdir(path):
         temp=os.path.join(path,item)
-        print(temp)
         if not os.path.isdir(temp):
             list.append({"text":item})
         else:
             list.append({"text":item,"children":tree(temp)})
     return list
-#print(tree('/data/pycharm/django/tasknew'))
-#print(json.dumps(tree('/data/pycharm/django/tasknew')))
-tup=(1,2,3,4,5,6,7,1,2)
-print(set(tup))
+print(json.dumps(tree('/data/pycharm/django/tasknew'),ensure_ascii=False))
